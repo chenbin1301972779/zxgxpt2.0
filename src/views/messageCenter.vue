11ce5674bbf9279abbbec519419b104bb12d468d
delk <2543607510@qq.com>
2020-09-03 15:13:52 +0800

    消息嵌入fr页面

--- src/views/messageCenter.vue
+++ src/views/messageCenter.vue
@@ -1,60 +1,39 @@
 <!-- 消息中心 -->
 <template>
 	<div class="message-center">
-		<div style="margin-bottom: 15px;">
+		<div style="margin-bottom: 5px;">
 			<el-breadcrumb separator-class="el-icon-arrow-right">
 			  <el-breadcrumb-item :to="{ path: '/homePage' }">首页</el-breadcrumb-item>
 			  <el-breadcrumb-item>消息中心</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
 		<div class="main-content">
-		<div class="content-item leftBox" style="margin-top: 40px;">
-		    <div class="title">
+		<div class="content-item leftBox" style="margin-top: 10px;">
+		    <!--<div class="title">
 		        <span class="icon"></span>
 		        <span>预警推送</span>
-		    </div>
+		    </div>-->
 		    <div class="main">
 		        <el-tabs v-model="activeAlarmTab" stretch>
-		            <el-tab-pane label="天眼查" name="1">
+		            <el-tab-pane label="实时预警" name="1">
 		                <div class="tab-content-wrapper">
-		                    <div class="tab-content" v-for="(item,index) in qichachaData" :key="index">
-		                        <p style="font-weight:bold;font-size:14px">
-		                            <span>{{item.company}}</span>
-		                            <span>{{item.level}}</span>
-		                        </p>
-		                        <p style="color:#999;font-size:12px">
-		                            <span>{{item.reason}}</span>
-		                        </p>
-		                        <p style="color:#617be3;font-size:12px">
-		                            <span>风险推送人：{{item.person}}</span>
-		                            <span>推送时间：{{item.time}}</span>
-		                        </p>
-		                    </div>
+							<iframe width="100%" :height="curHeight" frameborder="0" marginwidth="0" marginheight="5" :src="urlTab1"></iframe>
 		                </div>
-		
 		            </el-tab-pane>
-		            <el-tab-pane label="中诚信" name="2">
+		            <el-tab-pane label="风险早报" name="2">
 		                <div class="tab-content-wrapper">
-							<div style="text-align: right;font-size: 14px;color:#617be3;margin-bottom: 10px;cursor: pointer;" @click="gobi">查看更多>></div>
-							<div class="tab-content" v-for="(item,index) in zhongchengxinData" :key="index">
-							    <p style="font-size:14px;margin-bottom: 10px;">
-							        <span>{{item.companyName}}
-										<span style="background-color:#617be3;" class="status-style" v-if="item.status==1">一般</span>
-										<span style="background-color:#E6A23C;" class="status-style" v-else-if="item.status==2">重大</span>
-									</span>
-							    </p>
-							    <p style="font-size:14px">
-							        <span>风险事件类型：{{item.type}}</span>
-							        <span>最新推送时间：{{item.time}}</span>
-							    </p>
-							</div>
+							<iframe width="100%" :height="curHeight" frameborder="0" marginwidth="0" marginheight="5" :src="urlTab2"></iframe>
 						</div>
 		            </el-tab-pane>
-		            <el-tab-pane label="站内信" name="3">
-		                <div class="tab-content-wrapper">站内信</div>
+		            <el-tab-pane label="新闻早报" name="3">
+		                <div class="tab-content-wrapper">
+							<iframe width="100%" :height="curHeight" frameborder="0" marginwidth="0" marginheight="5" :src="urlTab3"></iframe>
+						</div>
 		            </el-tab-pane>
-		            <el-tab-pane label="集团公示" name="4">
-		                <div class="tab-content-wrapper">集团公示</div>
+		            <el-tab-pane label="平台消息" name="4">
+		                <div class="tab-content-wrapper">
+							<iframe width="100%" :height="curHeight" frameborder="0" marginwidth="0" marginheight="5" :src="urlTab4"></iframe>
+						</div>
 		            </el-tab-pane>
 		        </el-tabs>
 		    </div>
@@ -99,7 +78,16 @@
 						time:'2020-07-29'
 					}
 				],
+				curHeight:'',
+				urlTab1: `http://10.0.130.27:8080/webroot/decision/view/form?viewlet=Homepage/TYC_RISK.cpt&op=view&userCode=`+this.$Cookies.get('userCode'),
+				urlTab2: `http://10.0.130.27:8080/webroot/decision/view/form?viewlet=Homepage/ZCX_RISK.cpt&op=view&userCode=`+this.$Cookies.get('userCode'),
+				urlTab3: `http://10.0.130.27:8080/webroot/decision/view/form?viewlet=Homepage/ZCX_NEWS.cpt&op=view&userCode=`+this.$Cookies.get('userCode'),
+				urlTab4: `http://10.0.130.27:8080/webroot/decision/view/form?viewlet=Homepage/ZNX_PUSH.cpt&op=view&userCode=`+this.$Cookies.get('userCode')
 			}
+		},
+		mounted() {
+			var h = document.documentElement.clientHeight || document.body.clientHeight;
+			this.curHeight =h-230;
 		},
 		methods:{
 			gobi(){
@@ -125,7 +113,7 @@
 		padding: 20px;
 		overflow-block: auto;
 		.title {
-		    margin-bottom: 10px;
+		    margin-bottom: 5px;
 		    font-size: 14px;
 		    .icon {
 		        display: inline-block;
